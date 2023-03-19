e83eded6495507dff9001c9624fd0fb2fbf163da
Atishay Jain <aj@Atishays-MacBook-Air.local>
2023-03-19 09:51:34 +0100

    Pushing Changes

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,21 +1,33 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+from blog.config import Config
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '806bebb5cf5a6d654dbff4d268e4f585'
-app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(os.path.abspath(os.path.dirname(__file__)), 'site.db')}"
 
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
+mail = Mail()
 
-from blog import routes, models
 
-with app.app_context():
-    db.create_all()
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from blog.users.routes import users
+    from blog.posts.routes import posts
+    from blog.main.routes import main
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
